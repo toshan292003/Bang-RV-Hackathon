e75eb325c18c9ddb32c931bb50622eb8005e79db
toshan292003 <toshanyt@gmail.com>
2024-06-05 23:20:46 +0530

    ccbxn

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/navbar';
+import Home from './Pages/home';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar></Navbar>
+      <ToastContainer/>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
 export default App;
---
+++ Frontend/myapp/src/Pages/navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./navbar.css";
+export default function Navbar(){
+    return(
+        <>
+            <div className="navbox">
+                <div className="navitems">
+                    <span>Educaite</span>
+                </div>
+                <div className="navitems">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/services">Services</a>
+                        </li>
+                        <li>
+                            <a href="/input">Inputs</a>
+                        </li>
+                        <li>
+                            <a href="/about">About Us</a>
+                        </li>
+                        <li>
+                            <a href="/signin">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
